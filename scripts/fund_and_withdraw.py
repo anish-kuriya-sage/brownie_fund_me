5ae43f9c41ddd182c3a2b9a3d52b3f44d408d0ce
Anish Kuriya <Anish.Kuriya@sage.com>
2022-05-05 20:41:56 +0530

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,20 @@
+from brownie import FundMe
+from scripts.helpful_script import get_account
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(f"Entrance fee is {entrance_fee}")
+    print("Funding contract...")
+    fund_me.fund({'from': account, 'value': entrance_fee})
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    print("Withdrawing from contract...")
+    fund_me.withdraw({'from': account})
+
+def main():
+    fund()
+    withdraw()
